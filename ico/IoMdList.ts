e1d62b0c3bdd1a439602f4d1f0d0923cb706aa3e
urielch <uriel.chemouni@gmail.com>
2024-07-17 13:34:51 +0200

    order icons by name

--- ico/IoMdList.ts
+++ ico/IoMdList.ts
@@ -1,6 +1,6 @@
 import { GenIcon, type IconBaseProps } from "../deps.ts";
 
 export function IoMdList(props: IconBaseProps) {
-  return GenIcon({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M80 280h256v48H80zM80 184h320v48H80zM80 88h352v48H80z"}},{tag:"g",attr:{},child:[{tag:"path",attr:{d:"M80 376h288v48H80z"}}]}]})(props);
+  return GenIcon({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M80 280h256v48H80zM80 184h320v48H80zM80 88h352v48H80z"}},{tag:"g",attr:{},child:[{tag:"path",attr:{d:"M80 376h288v48H80z"},child:[]}]}]})(props);
 }
 export default IoMdList;
